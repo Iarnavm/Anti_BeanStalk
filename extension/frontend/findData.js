b963405e64633334840e676b13c687d158c41dfe
mandeep-mahra <mandeepmehara1@gmail.com>
2023-11-19 14:32:41 +0530

    extension

--- extension/frontend/findData.js
+++ extension/frontend/findData.js
@@ -2,6 +2,5 @@ export default async function hibpData(domainName) {
 	const response = await fetch("https://haveibeenpwned.com/api/v2/breach/" + domainName)
 	const data = await response.json();
 	console.log(data);
-	return data;
-	
+	return data;	
 }
