5da43f78591d18160f795fa9d6285dc3ae2a6bdd
Arshdeep Singh <arsh.01042002@gmail.com>
2023-11-19 17:26:14 +0530

    Aws Deployment, API ready

--- app.js
+++ app.js
@@ -6,32 +6,19 @@ const path = require('path')
 const axios = require('axios');
 const app = express();
 const helpers = require('./public/js/helper');
+const { type } = require("os");
 const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname , 'public')));
 
-
-// app.get('/proxy', async (request, response) => {
-//     const websiteURL = request.query.url;
-//     console.log(websiteURL)
-//     try {
-//       const proxyResponse = await axios.get(websiteURL);
-      
-//       Object.entries(proxyResponse.headers).forEach(([key, value]) => {
-//         response.setHeader(key, value);
-//       });
-  
-//       response.send(proxyResponse.data);
-//     } catch (error) {
-//       console.error(error);
-//       response.status(500).send('Error proxying the request');
-//     }
-//   });
-
-app.post('/upload', async(request, response) => {
-    const check = await helpers.testIfWebsitePhishy(request.body.websiteURL)
-    response.json({'phishingWebsite' : check});
+app.post('/', async (request, response) => {
+    // Extract the website URL from the request parameters
+    const websiteURL = request.body.websiteURL;
+    console.log(websiteURL)
+    // Call testIfPhishy with the extracted website URL
+    const check = await helpers.testIfWebsitePhishy(websiteURL);
+    response.json({'phishingWebsite': check});
 });
- 
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
