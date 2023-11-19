0f005b67ea007c79dcfc9582b1b18dd240d0f3fc
Avinash Yadav <avinashyadav.15@outlook.com>
2023-11-19 14:21:13 +0530

    added findData

---
+++ extension/frontend/findData.js
@@ -0,0 +1,7 @@
+export default async function hibpData(domainName) {
+	const response = await fetch("https://haveibeenpwned.com/api/v2/breach/" + domainName)
+	const data = await response.json();
+	console.log(data);
+	return data;
+	
+}
